e93adf9c65fa58bcee0986c0e4bf3ff324be1d06
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-25 22:37:01 +0530

    ChatContainer Working

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -4,9 +4,10 @@ import ChatHeader from "../components/ChatHeader";
 import MessageInput from "../components/MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { userAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { message, getMessages, isMessagesLoading, selectedUser } =
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
     useChatStore();
 
   const { authUser } = userAuthStore();
@@ -30,7 +31,7 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-         {message.map((message) => (
+         {messages.map((message) => (
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
