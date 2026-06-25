1bc68aee8a136947f9a75db6b43679d03afdd57f
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-26 02:45:51 +0530

    Update project structure: Add winkly package as a local dependency and adjust build/start scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,14 @@ import cookieParser from "cookie-parser";
 
 import {app,server} from "./lib/socket.js" 
 
+
+import path from "path";
+
 dotenv.config();
 // const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve()
 
 app.use(cookieParser()); 
 app.use(express.json({ limit: "50mb" }));
@@ -28,7 +32,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
 
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 server.listen(PORT, ()=> {
     console.log("server is running on PORT:" + PORT);
