bf50b1f0fa262148ddb8631461291838b1050f5e
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-26 23:35:12 +0530

    Revert "fix: update environment check for production in socket and auth store"
    This reverts commit 37894e3ac6efc2ace4387f7c8ab6cef7ee9a6d37.

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,7 +10,7 @@ const server =  http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin:
-      process.env.NODE_ENV === "production"
+      process.env.NODE_ENV === "development"
         ? "https://winkly-chat-app.onrender.com"
         : "http://localhost:5173",
     credentials: true,
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import {axiosInstance} from "../lib/axios.js"
 import {io} from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "production" ?"http://localhost:5001" : 'https://winkly-chat-app.onrender.com';
+const BASE_URL = import.meta.env.MODE === "development" ?"http://localhost:5001" : 'https://winkly-chat-app.onrender.com';
 export const userAuthStore = create((set, get) => ({
 authUser : null,
 isSigningUp : false,
